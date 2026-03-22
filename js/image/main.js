d896fe54481cb6ceee07348f07d3aad559cc6415
newhope8397 <nhope8397@gmail.com>
2026-03-23 00:21:52 +0530

    Update main.js

--- js/image/main.js
+++ js/image/main.js
@@ -1,17 +1,3 @@
-// ==========================
-// MAIN APP INIT
-// ==========================
 document.getElementById("upload").addEventListener("change", (e)=>{
     loadImage(e.target.files[0]);
 });
-
-document.getElementById("undoBtn").onclick = undo;
-document.getElementById("redoBtn").onclick = redo;
-
-document.getElementById("brightness").oninput = (e)=>{
-    applyBrightness(parseInt(e.target.value));
-};
-
-document.getElementById("grayscale").onclick = applyGrayscale;
-document.getElementById("cropBtn").onclick = startCrop;
-document.getElementById("download").onclick = downloadImage;
