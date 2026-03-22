0a1add65b3c38d7e92f7c1b0307535d249ab9d3c
newhope8397 <nhope8397@gmail.com>
2026-03-22 22:53:33 +0530

    Update drag.js

--- js/image/drag.js
+++ js/image/drag.js
@@ -1,1 +1,23 @@
+// ==========================
+// DRAG SYSTEM
+// ==========================
+canvas.addEventListener("mousedown", (e)=>{
+    app.isDragging = true;
+    app.startX = e.offsetX;
+    app.startY = e.offsetY;
+});
 
+canvas.addEventListener("mousemove", (e)=>{
+    if(!app.isDragging) return;
+
+    let dx = e.offsetX - app.startX;
+    let dy = e.offsetY - app.startY;
+
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    ctx.drawImage(app.currentImage, dx, dy);
+});
+
+canvas.addEventListener("mouseup", ()=>{
+    app.isDragging = false;
+    if(window.saveState) saveState();
+});
