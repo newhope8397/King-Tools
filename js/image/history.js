b42a73f1bd879697221abf87c61e8d609e15b0ba
newhope8397 <nhope8397@gmail.com>
2026-03-22 22:53:05 +0530

    Update history.js

--- js/image/history.js
+++ js/image/history.js
@@ -1,1 +1,33 @@
+// ==========================
+// HISTORY SYSTEM
+// ==========================
+let history = [];
+let redoStack = [];
 
+window.saveState = function(){
+    history.push(canvas.toDataURL());
+    if(history.length > 20) history.shift();
+    redoStack = [];
+}
+
+window.undo = function(){
+    if(history.length > 0){
+        redoStack.push(canvas.toDataURL());
+
+        let img = new Image();
+        img.src = history.pop();
+
+        img.onload = () => ctx.drawImage(img,0,0);
+    }
+}
+
+window.redo = function(){
+    if(redoStack.length > 0){
+        history.push(canvas.toDataURL());
+
+        let img = new Image();
+        img.src = redoStack.pop();
+
+        img.onload = () => ctx.drawImage(img,0,0);
+    }
+}
