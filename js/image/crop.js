e0d02320ace86a8bc1e42daf2a63fcf512ad644a
newhope8397 <nhope8397@gmail.com>
2026-03-22 22:54:07 +0530

    Update crop.js

--- js/image/crop.js
+++ js/image/crop.js
@@ -1,1 +1,30 @@
+// ==========================
+// CROP TOOL
+// ==========================
+let cropStart = null;
 
+function startCrop(){
+    canvas.addEventListener("mousedown", cropMouseDown);
+    canvas.addEventListener("mouseup", cropMouseUp);
+}
+
+function cropMouseDown(e){
+    cropStart = {x: e.offsetX, y: e.offsetY};
+}
+
+function cropMouseUp(e){
+    let endX = e.offsetX;
+    let endY = e.offsetY;
+
+    let width = endX - cropStart.x;
+    let height = endY - cropStart.y;
+
+    let imageData = ctx.getImageData(cropStart.x, cropStart.y, width, height);
+
+    canvas.width = width;
+    canvas.height = height;
+
+    ctx.putImageData(imageData, 0, 0);
+
+    saveState();
+}
