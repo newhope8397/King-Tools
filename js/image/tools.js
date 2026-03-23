3df59e84d8931d4c058567e694c499857ec0de9c
newhope8397 <nhope8397@gmail.com>
2026-03-23 15:59:16 +0530

    Update tools.js

--- js/image/tools.js
+++ js/image/tools.js
@@ -1,18 +1,5 @@
 // ==========================
-// BASIC FILTERS
-// ==========================
-function applyBrightness(value){
-    let imageData = ctx.getImageData(0,0,canvas.width,canvas.height);
-    let data = imageData.data;
-
-    for(let i=0;i<data.length;i+=4){
-        data[i] = Math.min(255, Math.max(0, data[i] + value));
-        data[i+1] = Math.min(255, Math.max(0, data[i+1] + value));
-        data[i+2] = Math.min(255, Math.max(0, data[i+2] + value));
-    }
-
-    ctx.putImageData(imageData,0,0);
-}
+// BASIC FILTERS 
 
 // ==========================
 // CONTROLS
@@ -20,36 +7,15 @@ function applyBrightness(value){
 
 // BRIGHTNESS SLIDER
 function setBrightness(val){
-    if(!app.originalImage) return;
-
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-
-    ctx.drawImage(app.originalImage, 0, 0, canvas.width, canvas.height);
-
-    applyBrightness(val - 100);
+    app.brightness = val;
+    render();
 }
 
-// CONTRAST
-    function setContrast(val){
-    if(!app.originalImage) return;
-
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-    ctx.drawImage(app.originalImage, 0, 0, canvas.width, canvas.height);
-
-    let imageData = ctx.getImageData(0,0,canvas.width,canvas.height);
-    let data = imageData.data;
-
-    let factor = (259 * (val + 255)) / (255 * (259 - val));
-
-    for(let i=0;i<data.length;i+=4){
-        data[i] = Math.min(255, Math.max(0, factor * (data[i]-128) + 128));
-        data[i+1] = Math.min(255, Math.max(0, factor * (data[i+1]-128) + 128));
-        data[i+2] = Math.min(255, Math.max(0, factor * (data[i+2]-128) + 128));
-    }
-
-    ctx.putImageData(imageData,0,0);
-    }
-
+//CONTRAST 
+function setContrast(val){
+    app.contrast = val;
+    render();
+}
 // ROTATE
 function rotate(){
     const tempCanvas = document.createElement("canvas");
@@ -71,27 +37,15 @@ function rotate(){
 }
 
 // ZOOM
-let zoomLevel = 1;
-
-function zoom(scale){
-    if(!app.currentImage) return;
-
-    zoomLevel = scale;
-
-    ctx.clearRect(0,0,canvas.width,canvas.height);
-
-    let w = app.currentImage.width * zoomLevel;
-    let h = app.currentImage.height * zoomLevel;
-
-    let x = (canvas.width - w) / 2;
-    let y = (canvas.height - h) / 2;
-
-    ctx.drawImage(app.currentImage, x, y, w, h);
+function zoom(val){
+    app.zoom = val;
+    render();
 }
+
 document.getElementById("zoomRange").addEventListener("input", (e)=>{
     zoom(parseFloat(e.target.value));
 });
-// RESIZE
+// RESIZE 
 function resizeImg(){
     let w = document.getElementById("w").value;
     let h = document.getElementById("h").value;
